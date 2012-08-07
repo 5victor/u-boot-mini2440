b4e95c24f3e6aeb161d39f6fe8ba05dc631e36e7
Victor Wen <vic7tor@gmail.com>
2012-08-07 13:27:12 +0800

    u-boot-spl can load from nand flash

--- arch/arm/cpu/arm920t/spl.c
+++ arch/arm/cpu/arm920t/spl.c
@@ -1,9 +1,11 @@
 #include <common.h>
 #include <nand.h>
 
+static gd_t gdata __attribute__ ((section("data")));
+
 void board_init_f(ulong dummy)
 {
-
+	relocate_code(CONFIG_SPL_STACK, &gdata, CONFIG_SPL_TEXT_BASE);
 }
 
 void board_init_r(gd_t *id, ulong dest_addr)
